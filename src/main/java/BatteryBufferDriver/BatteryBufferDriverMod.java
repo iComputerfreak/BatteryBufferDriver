ae0b68b12284f3195de45a16c5e728580cfe958d
Jonas Frey <uni@jonasfrey.de>
2019-05-22 12:42:35 +0200

    Increased mod version

--- src/main/java/BatteryBufferDriver/BatteryBufferDriverMod.java
+++ src/main/java/BatteryBufferDriver/BatteryBufferDriverMod.java
@@ -13,7 +13,7 @@ public class BatteryBufferDriverMod
 
 	public static final String MODID = "batterybufferdriver";
 	public static final String NAME = "BatteryBufferDriver";
-	public static final String VERSION = "1.1";
+	public static final String VERSION = "1.2";
 
 
 	private static Logger logger;
